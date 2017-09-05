396aff45381456d1e8b92fc9a6e4769f0cb85361
Alex Nichol <unixpickle@gmail.com>
2017-09-05 16:15:29 -0700

    remove meta environment for now
    I am going to put this into anyrl instead, since it could easily be more general.

--- wrappers.go
+++ wrappers.go
@@ -1,8 +1,6 @@
 package mazenv
 
 import (
-	"errors"
-
 	"github.com/unixpickle/anyvec"
 )
 
@@ -51,60 +49,4 @@ func (s *SurroundingsEnv) observe() anyvec.Vector {
 	grid := oneHotGrid(s.Maze(), p, startRow, startCol, size, size)
 	vecData := s.Creator().MakeNumericList(grid)
 	return s.Creator().MakeVectorData(vecData)
-}
-
-// MetaEnv creates a meta-learning environment around Env.
-//
-// Each meta-episode consists of NumRuns episodes of Env.
-// The action space is the same, but the observations are
-// augmented (at the end) with the previous action, the
-// reward, and the done boolean (in that order).
-//
-// For the first observation, the action and reward values
-// are both 0.
-type MetaEnv struct {
-	Env
-	NumRuns int
-
-	runsRemaining int
-}
-
-// Reset resets the environment.
-func (m *MetaEnv) Reset() (obs anyvec.Vector, err error) {
-	m.runsRemaining = m.NumRuns
-	obs, err = m.Env.Reset()
-	if err != nil {
-		return
-	}
-	zeroVec := obs.Creator().MakeVector(6)
-	obs = obs.Creator().Concat(obs, zeroVec)
-	return
-}
-
-// Step takes a step in the environment.
-func (m *MetaEnv) Step(act anyvec.Vector) (obs anyvec.Vector, rew float64,
-	done bool, err error) {
-	if m.runsRemaining <= 0 {
-		err = errors.New("step: done sub-episodes in meta-environment")
-		return
-	}
-	obs, rew, done, err = m.Env.Step(act)
-	if err != nil {
-		return
-	}
-	rewDoneVec := []float64{rew, 0}
-	if done {
-		rewDoneVec[1] = 1
-		m.runsRemaining--
-		done = m.runsRemaining == 0
-		if !done {
-			obs, err = m.Env.Reset()
-			if err != nil {
-				return
-			}
-		}
-	}
-	c := obs.Creator()
-	obs = c.Concat(obs, act, c.MakeVectorData(c.MakeNumericList(rewDoneVec)))
-	return
 }
